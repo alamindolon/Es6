f96f921e48efde4ce09a1d8dfc45c2e6d6d218a9
Al Amin Sarker Dolon <80084331+alamindolon@users.noreply.github.com>
2023-03-11 00:51:16 +0600

    templete

---
+++ templete.js
@@ -0,0 +1,7 @@
+const a = 20;
+const b= 30;
+const summery = 'sum of: '+ a + ' and ' + b + ' is ' +(a+b);
+
+console.log(summery);
+ const newsummery  = `  sum of ${a} and ${b} is : ${a+b}`
+ console.log(newsummery);
