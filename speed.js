d169ae32324403449bccc9aa78b28c67fa635bc5
Al Amin Sarker Dolon <80084331+alamindolon@users.noreply.github.com>
2023-03-11 08:56:09 +0600

    speed added...

---
+++ speed.js
@@ -0,0 +1,17 @@
+
+// ... dile array gula ke numver a convert kore.
+const number = [23,45,67,78,99];
+const numbers = [23, ...number, 74,56];
+console.log(numbers);
+
+
+// first a number banay nise than max numver banaise 
+const highest = Math.max(...number);
+console.log(highest);
+
+
+// ... na dile numver a convert hobe na . so ai ta kaj korbe na.
+const highest = Math.max(number);
+console.log(highest);
+
+
